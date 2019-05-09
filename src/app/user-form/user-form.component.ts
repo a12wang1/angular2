04d9e16c408bb7151d1df88cd1400956e19233ba
a12wang1 <a12wang@126,com>
2019-05-09 17:24:20 +0800

    2019/05/09 17:23 P133 5.5响应式表单FormBuilder

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -2,13 +2,33 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-form',
-  templateUrl: './user-form.component.html',
+  template: `
+  <div class="ui rasied segment">
+	  <h2 class="ui header"> Demo</h2>
+	  <form #f="ngForm" 
+		  (ngSubmit) = "onSubmit(f.value)"
+		  class = "ui form">
+		  <div class= "field">
+			  <label for="skuInput">SKU</label>
+			  <input type="text"
+				  id="skuInput"
+				  placeholder="SKU"
+				  name="sku" ngModel>
+			  </div>
+			  <button type="submit" class="ui button">button</button>
+		  </form>
+	  </div>
+  `,
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
 
   constructor() { }
-
+  
+  onSubmit(form: any): void {
+	  console.log(form)
+	  }
+  	  
   ngOnInit() {
   }
 
