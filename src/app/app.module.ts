1004788013f7097fbd93931a3edca0597dad8052
a12wang1 <a12wang@126,com>
2019-05-09 17:02:25 +0800

    2019/05/09 17:01

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloWorldComponent,
+    UserItemComponent,
+    UserListComponent,
+    UserFormComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
